e234d276745d4fe0db26847f34f2bb351ba2d7fd
Mykola Matsko <mykola.matsko@gmail.com>
2020-04-20 23:31:30 +0300

    account servise

---
+++ src/app/loggining.service.ts
@@ -0,0 +1,6 @@
+
+export class LogginingService {
+  logStatusChange(status: string) {
+    console.log('A server status changed, new status ' + status );
+  }
+}
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {LogginingService} from '../loggining.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LogginingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LogginingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
